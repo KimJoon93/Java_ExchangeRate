b503e2e868abfe107c3bc27bfc88b717d27569d3
Kim Joon <jk1094@naver.com>
2019-12-29 23:51:04 +0900

    dayOfWeek

--- ExchangeRate.java
+++ ExchangeRate.java
@@ -8,15 +8,30 @@ import java.util.Calendar;
 
 public class ExchangeRate {
 
-
     private final String USER_AGENT = "Mozilla/5.0";
-
+    
     public static void main(String[] args) {
         
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyyMMdd");
+        String searchDate = dateformat.format(System.currentTimeMillis());
+        
+        Calendar cal = Calendar.getInstance();
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        
+        if(dayOfWeek == 7){
+            // Saturday
+            searchDate = Integer.toString(Integer.parseInt(searchDate) -1);
+        }else if(dayOfWeek == 1){
+            // Sunday
+            searchDate = Integer.toString(Integer.parseInt(searchDate) -2);
+        }
+        String AUTHKEY = "";
         ExchangeRate http = new ExchangeRate();
+        String baseUrl = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?";
+        String fullUrl = baseUrl + "authkey=" + AUTHKEY + "&searchdate=" + searchDate + "&data=AP01";
+        
         try {
-            http.sendGET(
-                    "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey="인증키"&searchdate=20191227&data=AP01");
+            http.sendGET(fullUrl);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -43,7 +58,5 @@ public class ExchangeRate {
         
         System.out.println("HTTP 응답 코드 : " + responseCode); 
         System.out.println("HTTP body : " + response.toString());
-
     }
 }
-
